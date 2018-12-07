e342d2fe71e4274d7dc730cd419582ac2c8f5bcd
Linus <linus.manser@gmail.com>
2018-12-07 16:37:02 +0100

    cleaned up some whitespaces

--- stats.py
+++ stats.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 # Author: Linus Manser, 13-791-132
-# date: 20.10.2018
+# date: 07.12.2018
 # Additional Info: python3 (3.6.2) program
+#                  This file is not of any importance anymore, since most of the
+#                  statistical calculations were done by predefined functions
+#                  such as the confusion matrix, used for evaluation
+
 
 import matplotlib
 import matplotlib.pyplot as plt
@@ -12,19 +16,14 @@ import os
 import re
 import pandas as pd 
 from collections import defaultdict
-from run import load_class_mapping, load_class_mapping_pd
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
 from statsmodels.formula.api import ols
 from matplotlib.ticker import MaxNLocator
+from Classes import reverse_mapping, load_class_mapping_pd
 
 class Datasheet(object):
     def __init__(self, filename, male_mapping, female_mapping):
-        self.indexlist = ["age", "gender", "mfcc1_sdc", "mfcc2_sdc", "mfcc3_sdc", "mfcc4_sdc", "mfcc5_sdc", "mfcc6_sdc", "mfcc7_sdc", "mfcc8_sdc", "mfcc9_sdc", "mfcc10_sdc", "mfcc11_sdc", "mfcc12_sdc", "mfcc13_sdc",\
-                        "mfcc1_d_sdc", "mfcc2_d_sdc", "mfcc3_d_sdc", "mfcc4_d_sdc", "mfcc5_d_sdc", "mfcc6_d_sdc", "mfcc7_d_sdc", "mfcc8_d_sdc", "mfcc9_d_sdc", "mfcc10_d_sdc", "mfcc11_d_sdc", "mfcc12_d_sdc", "mfcc13_d_sdc", \
-                        "mfcc1_dd_sdc", "mfcc2_dd_sdc", "mfcc3_dd_sdc", "mfcc4_dd_sdc", "mfcc5_dd_sdc", "mfcc6_dd_sdc", "mfcc7_dd_sdc", "mfcc8_dd_sdc", "mfcc9_dd_sdc", "mfcc10_dd_sdc", "mfcc11_dd_sdc", "mfcc12_dd_sdc", "mfcc13_dd_sdc", \
-                        "pitch_stdev", "pitch_min", "pitch_max", "pitch_range", "pitch_med", "jit_loc", "jit_loc_abs", "jit_rap", "jit_ppq5", "jit_ddp", "shim_loc", "shim_apq3","shim_apq5","shim_dda", "vlhr", "stilt", "skurt", "scog", "bandenergylow","bandenergyhigh","deltaUV","meanUV","varcoUV","speakingrate","speakingratio", \
-                        "ff1", "ff2", "ff3", "ff4", "f1amp", "f2amp", "f3amp", "f4amp", "I12diff", "I23diff", "harmonicity", "f0"]
         self.filename = filename
         self.male_mapping = male_mapping
         self.female_mapping = female_mapping
@@ -36,10 +35,6 @@ class Datasheet(object):
         m_min_age = self.male_mapping["lowerbound"][0]
         m_max_age = self.male_mapping["upperbound"][m_max_index]
         bad_indices = []
-        # sel = df[(df["age"] < m_min_age) | (df["age"] > m_max_age)]
-        # for i in sel.index:
-        #     bad_indices.append(i)
-        # df = df.drop(set(bad_indices))
         ages = df["age"].tolist()
         genders = df["gender"].tolist()
         assert(len(ages)==len(genders))
@@ -60,14 +55,14 @@ class Datasheet(object):
                     except KeyError:
                         age_classes.append((i, 1337.1))
             except IndexError:
-                pass        
+                pass
         for i, ac in age_classes:
             df.at[i, "age_class"] = ac
-        
+
         for name in df:
             df[name] = df[name].astype('float64')
         return df
-        
+
     def corr(self, method="pearson", gender=None):
         if not gender:
             return self.data.corr(method)
@@ -76,7 +71,38 @@ class Datasheet(object):
             return m.corr(method)
         else:
             f = self.data.loc[self.data["gender"] == 1]
-            return f.corr(method)            
+            return f.corr(method)
+
+    def get_target_and_training_data(self):
+        out_df = self.data
+        out_df = out_df.dropna()
+        male, female = out_df.groupby("gender")
+        male = male[1].drop(["gender"], axis=1)
+        self.male_target = male["age_class"]
+        self.male = male.drop(["age", "age_class"], axis=1)
+        female = female[1].drop(["gender"], axis=1)
+        self.female_target = female["age_class"]
+        self.female = female.drop(["age", "age_class"], axis=1)
+
+    def apply_chi2(self, k=10, gender="m"):
+        X = self.male
+        y = self.male_target
+        if gender == "f":
+            X = self.female
+            y = self.female_target
+
+        chi2_selector = SelectKBest(chi2, k=k)
+        X_kbest_chi2 = chi2_selector.fit_transform(X, y)
+        feature_list = find_corresponding_name(X_kbest_chi2, X)
+        print("Feature list from chi-squared test:\n{}".format(feature_list))
+
+    def apply_anova_fval(self, k=10, gender="m"):
+        X = self.male
+        y = self.male_target
+        fvalue_selector = SelectKBest(f_classif, k=k)
+        X_kbest_fvalue = fvalue_selector.fit_transform(X, y)
+        feature_list = find_corresponding_name(X_kbest_fvalue, X)
+        print("Feature list from ANVOA F-Value test:\n{}".format(feature_list))
 
     def __repr__(self):
         return str(self.data)
@@ -85,67 +111,89 @@ class PopPlot(object):
     def __init__(self, datasheet):
         self.ds = datasheet
         self.df = self.ds.data
-        
-    def age_samples(self):
+
+    def by_age(self, min_age=20, max_age=85):
         x = []
         y = []
-        for age in range(26, 85):
+        for age in range(min_age, max_age):
             count = 0
             x.append(age)
             tmp = self.df.loc[self.df["age"] == float(age)]
             y.append(len(tmp))
-            
+
         fig, ax = plt.subplots()
         ax.bar(x, y)
         ax.xaxis.set_major_locator(MaxNLocator(integer=True))
         plt.show()
-            
-            
-        
-        
-class ScatterFeaturePlot(object):
-    def __init__(self, datasheet):
-        self.ds = datasheet
-        self.df = datasheet.data
-        self.pvals_male = pvals_male
-        self.pvals_female = pvals_female
-        self.colors = ["#2C7BB6", "#D7191C"]
 
-    def scatterplot(self, names, t=0.05):
-        names = list(names)
-        xval = 0
-        x_male = []
-        y_male = []
-        x_female = []
-        y_female = []
-        for name in names:
-            xval += 1
-            y_male.append(abs(self.pvals_male[name]))
-            y_female.append(abs(self.pvals_female[name]))
-            x_male.append(name)
-            x_female.append(name)
-        
+    def by_age_class(self, min_age=20, max_age=85):
+        x = []
+        y = []
+        for ac in range(0,5):
+            count = 0
+            x.append(ac)
+            tmp = self.df.loc[self.df["age_class"] == float(ac)]
+            y.append(len(tmp))
+
         fig, ax = plt.subplots()
-        ax.scatter(x_male, y_male, color=self.colors[0])
-        ax.scatter(x_female, y_female, color=self.colors[1])
-        ax.plot([-10,10], [t, t], color="red")
+        ax.bar(x, y)
         ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-        plt.xticks(rotation=90)
         plt.show()
-        
-        
-    
+
 class LongitudinalFeaturePlot(object):
     def __init__(self, datasheet):
         self.datasheet = datasheet
         self.df = datasheet.data
         self.labels = []
         self.colors = ["#2C7BB6", "#D7191C"] # 0 male, 1 female
+
+    def scatterplot(self, name, mode="mean", max_age=85, min_age=20):
+        d = self.df.loc[self.df["age"] >= min_age]
+        d = d.loc[d["age"] <= max_age]
+        M, F = d.groupby("gender")
+        male_df = M[1]
+        female_df = F[1]
+        male_x = male_df["age_class"].tolist()
+        female_x = female_df["age_class"].tolist()
+        male_y = male_df[name].tolist()
+        female_y = female_df[name].tolist()
+        male_labels = list(male_df)
+        female_labels = list(female_df)
+        male_mean_values = []
+        female_mean_values = []
+        male_set_x = []
+        female_set_x = []
+        male_m_x = []
+        female_m_x = []
+        for age in set(male_x):
+            male_set_x.append(age)
+            v = male_df.loc[male_df["age_class"] == age][name].tolist()
+            if mode == "mean":
+                male_m_x.append(np.mean(v))
+            elif mode == "median":
+                male_m_x.append(np.median(v))
         
+        for age in set(female_x):
+            female_set_x.append(age)
+            v = female_df.loc[female_df["age_class"] == age][name].tolist()
+            if mode == "mean":
+                female_m_x.append(np.mean(v))
+            elif mode == "median":
+                female_m_x.append(np.median(v))
+
+        fig, ax = plt.subplots()
+        ax.scatter(male_x, male_y, color="blue", alpha=0.5)
+        ax.scatter(female_x, female_y, color="red", alpha=0.5)
+        ax.plot(male_set_x, male_m_x)
+        ax.plot(female_set_x, female_m_x)
+        ax.set_title(name)
+        ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+        plt.show()
+
     def lineplot(self, name, mode="mean", by="age"):
         f, m = self.df.groupby("gender")
-        m_labels = list(set(m[1][by]))
-        f_labels = list(set(f[1][by]))
+        m_labels = sorted(list(set(m[1][by])))
+        f_labels = sorted(list(set(f[1][by])))
         d = {"m":{}, "f":{}}
         for l in m_labels:
             if mode == "mean":
@@ -153,23 +201,45 @@ class LongitudinalFeaturePlot(object):
             elif mode == "median":
                 v = np.median(m[1].loc[m[1][by] == l][name].values)
             d["m"][l] = v
-            
+
         for l in f_labels:
             if mode == "mean":
                 w = np.mean(f[1].loc[f[1][by] == l][name].values)
             elif mode == "median":
                 w = np.median(f[1].loc[f[1][by] == l][name].values)
             d["f"][l] = w
-            
-        fig, ax = plt.subplots()        
-        ax.plot(m_labels, [d["m"][x] for x in m_labels], color=self.colors[0])
-        ax.plot(f_labels, [d["f"][x] for x in f_labels], color=self.colors[1])
+
+        fig, ax = plt.subplots()
+        x_m = m_labels
+
+        exit()
+        y_m = [d["m"][x] for x in m_labels]
+        x_f = f_labels
+        y_f = [d["f"][x] for x in f_labels]
+        ax.scatter(x_m, y_m, color=self.colors[0])
+        ax.scatter(x_f, y_f, color=self.colors[1])
+        z = np.polyfit(x_m, y_m, 1)
+        print(z)
+        exit()
+
+        # ax.plot(x_m,p(x_m),"r--")
+
+        # Calculate the slope and y-intercept of the trendline
+        print("XM", x_m)
+        print("YM", y_m)
+        # clean up x_m (get rid of NaN values)
+        # fit = np.polyfit(x_m,y_m,1)
+        # print(fit)
+        # # Add the trendline
+        # yfit = [n*fit[0] for n in x_m]+fit[1]
+        # print(yfit)
+        # plt.scatter(x_m,y_m)
+        # plt.plot(yfit,'black')
         ax.set_title(name)
         ax.xaxis.set_major_locator(MaxNLocator(integer=True))
         plt.show()
-        
 
-    def boxplot(self, name):
+    def boxplot(self, name, medianline=False):
         """
         test boxplot
         """
@@ -192,17 +262,19 @@ class LongitudinalFeaturePlot(object):
             f_labels.append(label)
             x = s[name].values
             f_values.append(x)
-        
+
         if f_labels == m_labels:
             self.labels = m_labels
-            
+
         bplot1 = ax1.boxplot(m_values, labels=m_labels, meanline=True, showfliers=False, medianprops=medianprops)
-        # plt.setp(bplot1["boxes"], color="#2C7BB6")
         ax1.set_title("male")
         bplot1 = ax2.boxplot(f_values, labels=f_labels, meanline=True, showfliers=False, medianprops=medianprops)
         ax2.set_title("female")
+        if medianline:
+            ax1.plot([i + 1 for i in m_labels], [np.median(v) for v in m_values], "b--")
+            ax2.plot([j + 1 for j in f_labels], [np.median(v) for v in f_values], "r--")
         plt.show()
-    
+
     def grouped_boxplot(self, name):
         """
         test boxplot
@@ -228,84 +300,30 @@ class LongitudinalFeaturePlot(object):
         plt.show()
 
 
-def find_pearson(ds, features, base="age"):
-    p = ds.corr(method="pearson", gender="f")
-    q = ds.corr(method="pearson", gender="m")
-    male = {}
-    female = {}
-    for f in features:
-        female[f] = p.at[base, f]
-        male[f] = q.at[base, f]
-        
-    return male, female
-    
-def reverse_mapping(mapping):
-    d = {}
-    for index in mapping.index:
-        for age in range(mapping["lowerbound"].get(index), mapping["upperbound"].get(index)+1):
-            d[age] = index
-    return d
-
-def prepare_datafile(filename):
-    outname = filename[:-4] + "_clean.txt"
-    lines = 0
-    faulty_lines = 0
-    with open(outname, "w") as w:
-        with open(filename, "r") as r:
-            for line in r:
-                lines += 1
-                ignore_line = False
-                line_split = line.split(",")
-                for i in range(0,len(line_split)):
-                    line_split[i] = line_split[i].strip()
-                    if line_split[i] == "male":
-                        line_split[i] = "m"
-                    elif line_split[i] == "female":
-                        line_split[i] = "f"
-                    elif line_split[i] == "--undefined--":
-                        ignore_line = True
-                if not ignore_line:
-                    out_line = ",".join(line_split) + "\n"
-                    w.write(out_line)
-                else:
-                    faulty_lines += 1
-    print(" ! removed %i/%i lines because of undefined values" % (faulty_lines, lines))
-    return outname
-
-
 def main():
     APPDATA_PATH = os.getcwd()[:-4] + "appdata/" # assumes this script is in ~/code
     DATA_PATH = APPDATA_PATH + "fval/"
     MAPPING_PATH = APPDATA_PATH + "mappings/"
+
     print(" + loading age class mapping")
     m_mapping = f_mapping = load_class_mapping_pd(MAPPING_PATH + "f_mapping.txt")
-    # print(" + preparing datafile")
-    # datafile = prepare_datafile(DATA_PATH + "t.txt")
+
     print(" + setting up datasheet")
     ds = Datasheet(DATA_PATH + "extracted_fvals.txt", m_mapping, f_mapping)
-    print(ds)
-    print(" + computing pearson correlation")
-    p_male, p_female = find_pearson(ds, ds.indexlist[2:])
-    
-    # quick output of "significant" features
-    print("M")
-    for f, v in p_male.items():
-        if abs(v) < 0.05:
-            print("%10s\t%f" % (f,v))
-    print("F")
-    for f, v in p_female.items():
-        if abs(v) < 0.05:
-            print("%10s\t%f" % (f,v))
+    ds.get_target_and_training_data()
+    ds.apply_chi2(k=10)
+    ds.apply_anova_fval(k=10)
 
-    # long = LongitudinalFeaturePlot(ds)
-    # scatter = ScatterFeaturePlot(p_male, p_female)
-    # scatter.scatterplot(p_male.keys())
-    # long.lineplot("pVO")
-    # long.lineplot("pVO", mode="mean", by="age_class")
-    # long.boxplot("pVO")
-    pl = PopPlot(ds)
-    pl.age_samples()
-
+    print(" + plotting feature data")
+    plot = LongitudinalFeaturePlot(ds)
+    plot.boxplot("pVO", medianline=True)
+    plot.boxplot("F0_sdc_1", medianline=True)
+    plot.boxplot("F0_sdc_2", medianline=True)
+    plot.boxplot("F0_sdc_3", medianline=True)
+    plot.boxplot("harmonicity_sdc_3", medianline=True)
+    pop = PopPlot(ds)
+    pop.by_age()
+    pop.by_age_class()
 
 if __name__ == '__main__':
     main()
