f43e120ecbeb994e340a6195e55c6b67493bff1b
Linus <linus.manser@gmail.com>
2018-12-10 16:19:57 +0100

    added paths.py to make path errors in only one place, instead of every possible file

---
+++ paths.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+# Author: Linus Manser, 13-791-132
+# date: 03.12.2018
+# Additional Info: python3 (3.6.2) program
+
+        
+os.chdir("..")
+ROOT_PATH = os.getcwd() + "/"
+DB_PATH = ROOT_PATH + "AgeingDatabaseReleaseII/"
+APPDATA_PATH = ROOT_PATH + "appdata/"
+TRAIN_PATH = APPDATA_PATH + "train/*.txt"
+TEST_PATH = APPDATA_PATH + "test/*.txt"
+MALE_TRAIN_PATH = APPDATA_PATH + "train/m_train.csv"
+FEMALE_TRAIN_PATH = APPDATA_PATH + "train/f_train.csv"
+MALE_TEST_PATH = APPDATA_PATH + "test/m_test.csv"
+FEMALE_TEST_PATH = APPDATA_PATH + "test/f_test.csv"
+SEGMENT_PATH = APPDATA_PATH + "segments/"
+MAPPING_PATH = APPDATA_PATH + "mappings/"
+RECORDING_PATH = APPDATA_PATH + "recordings/"
+EXTRACTION_PATH = APPDATA_PATH + "extractions/"
+FVAL_PATH = APPDATA_PATH + "fval/"
+PROGRESS_PATH = APPDATA_PATH + "progress/"
+MODELS_PATH = APPDATA_PATH + "models/"
+PROGRESS_FILE_PATH = PROGRESS_PATH + "progress.txt"
+FEMALE_MAPPING_FILE_PATH = APPDATA_PATH + "mappings/f_mapping.txt"
+MALE_MAPPING_FILE_PATH = APPDATA_PATH + "mappings/m_mapping.txt"
+
+def main():
+    """
+    This is the collection of created paths. These paths are needed for
+    collecting the relevant data when setting up the system.
+    """
+    print("Hello form paths.py")
+    
+if __name__ == '__main__':
+    main()
